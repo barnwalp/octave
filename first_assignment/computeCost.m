61b067b62864a1a886bd6e8f4c9e601db825c9ed
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-09-13 18:06:39 +0530

    feat/plot of cost function with iteration

--- first_assignment/computeCost.m
+++ first_assignment/computeCost.m
@@ -12,7 +12,7 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-fprintf('\nchecking theta from computeCost function: %f', theta)
+%fprintf('\nchecking theta from computeCost function: %f', theta)
 J = sum((X*theta - y) .^ 2) / (2*m);
 
 % =========================================================================
--- first_assignment/test.m
+++ first_assignment/test.m
@@ -2,17 +2,26 @@ data = load('ex1data1.txt');
 
 m = length(data);
 X = [ones(m,1), data(:, 1)];
+%disp(J);
 y = data(:,2);
+l = [1:1500];
+
+plot(l, y);
+%hold on;
 
 theta = [0; 0];
 alpha = 0.01;
 iteration = 1500;
+J = [zeros(iteration,1)];
 
 for iter = 1:iteration
 	temp1 = theta(1) - (alpha/m)*(sum(X*theta - y));
 	temp2 = theta(2) - (alpha/m)*(sum((X*theta - y) .* X(:,2)));
 	theta = [temp1; temp2];
-
+	J(iter) = sum((X*theta - y) .^ 2) / (2*m);
 end
 
+fprintf('\nlength of J matrix is: %0.0f\n', length(J));
+%disp(J);
+plot(l, J);
 fprintf('Final theta value is: %f', theta);
