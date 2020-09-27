e41d8549a71370f713676f18a6229375a884797d
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-09-27 19:58:14 +0530

    feat/ predicting the numbers using neural network

--- third_assignment/predict.m
+++ third_assignment/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,13 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m,1), X];
+hidden_val = sigmoid(X * Theta1');
 
+k = size(hidden_val, 1);
+hidden_val = [ones(k, 1),  hidden_val];
+output_val = sigmoid(hidden_val * Theta2');
 
-
-
-
-
-
+[x, ix] = max(output_val, [], 2);
+p = ix;
+fprintf('\n %f \n', p)
 
 % =========================================================================
 
